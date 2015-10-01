7887dd0c5f0000168fae6526b13fc709e7d6c65c
Kyle <kyle@simspace.com>
2015-10-01 17:16:55 -0400

    theme support in the python builder

--- build.py
+++ build.py
@@ -5,21 +5,19 @@ import subprocess
 import os
 import sys
 
-flags = '--standalone -t revealjs --section-divs --template=libs/template.html --no-highlight'
+def compile_file(path, theme):
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser();
-    parser.add_argument('-n', '--name', help='Name of the markdown file', required=True)
-
-    args = parser.parse_args()
+    if not os.path.exists('./libs/css/theme/%s.css' % theme):
+        print '[!] error: no style sheet found: ./libs/css/theme/%s.css' % theme
+        return -1
 
     if not os.path.exists('./markdown'):
         print '[!] error: no markdown folder found'
-        sys.exit(1)
+        return -1
 
-    if not os.path.exists('./markdown/%s.md' % args.name):
-        print '[!] error: markdown file not found'
-        sys.exit(1)
+    if not os.path.exists('./markdown/%s.md' % path):
+        print '[!] error: markdown file not found: %s' % path
+        return -1
 
     if not os.path.exists('./slides'):
         os.mkdir('./slides')
@@ -28,13 +26,22 @@ if __name__ == "__main__":
            '--standalone',
            '-t',
            'revealjs',
+           '--variable=theme:%s' % theme,
            '--section-divs',
            '--template=libs/template.html',
            '--no-highlight']
-    
-    cmd.append('markdown/%s.md' % args.name)
+
+    cmd.append('markdown/%s.md' % path)
     cmd.append('-o')
-    cmd.append('slides/%s.html' % args.name)
+    cmd.append('slides/%s.html' % path)
     subprocess.check_call(cmd)
     print '[+] success: reveal html file created, ./slides/%s.html' % args.name
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser();
+    parser.add_argument('-n', '--name', help='Name of the markdown file', required=True)
+    parser.add_argument('-t', '--theme', help='Name of the css theme', required=False, default='moon')
+
+    args = parser.parse_args()
+
+    compile_file(args.name, args.theme)
